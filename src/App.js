1763ca445fcca13046352c3a974084988deb9947
nyeongsik13 <139778217+nyeongsik13@users.noreply.github.com>
2024-03-29 21:50:13 +0900

    FIX: app.js login and signup page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage.js';
+import OrderPage from './pages/OrderPage.js'
 import Icon from './components/Icon.js';
 function App() {
   return (
     <Router>
       <Icon type="MainPage" />
       <Routes>
-        <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/order" element={<OrderPage />} />
+          <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
   );
