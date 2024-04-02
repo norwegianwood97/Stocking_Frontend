26cf530a0e65c58b081c10d53ec79b3e9ca5667c
jovid_18 <cshcho99@gmail.com>
2024-04-02 15:57:57 +0900

    FEAT: add conditional rendering

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -4,24 +4,28 @@ import './OrderBook.css'; // CSS 스타일시트 임포트
 
 function OrderBook() {
   const [orderBook, setOrderBook] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); // 로딩 상태 관리
   const { companyId } = useParams();
 
   useEffect(() => {
+    setIsLoading(true); // 데이터 요청 시작
     const ws = new WebSocket(`ws://localhost:8090?companyId=${companyId}`);
 
     ws.onmessage = (event) => {
       const message = JSON.parse(event.data);
 
       if (Array.isArray(message)) {
-        const maxQuantity = Math.max(...message.map((order) => order._sum.quantity)); // 최대 수량 계산
+        const maxQuantity = Math.max(...message.map((order) => order._sum.quantity));
         const processedOrders = message.map((order) => ({
           ...order,
-          barLength: (order._sum.quantity / maxQuantity) * 100, // 비율 계산 후 백분율로 변환
+          barLength: (order._sum.quantity / maxQuantity) * 100,
         }));
 
         setOrderBook(processedOrders);
+        setIsLoading(false); // 데이터 로딩 완료
       } else {
         console.warn('Received data is not an array:', message);
+        setIsLoading(false);
       }
     };
 
@@ -29,8 +33,9 @@ function OrderBook() {
   }, [companyId]);
 
   return (
-    <div>
-      <h2>Order Book</h2>
+    <div className="orderBook">
+      <h2>호가창</h2>
+
       <table>
         <thead>
           <tr>
@@ -39,6 +44,7 @@ function OrderBook() {
             <th>매수주문</th>
           </tr>
         </thead>
+        {isLoading && <div className="loading-indicator">Loading...</div>} {/* 로딩 인디케이터 추가 */}
         <tbody>
           {orderBook.map(
             (order, index) =>
@@ -48,7 +54,7 @@ function OrderBook() {
                     <div className={`bar sell-bar ${order.type === 'sell' ? 'visible' : ''}`} style={{ width: `${order.type === 'sell' ? order.barLength : 0}%` }}></div>
                     <span className={`order-quantity ${order.type === 'sell' ? '' : 'order-quantity-buy'}`}>{order.type === 'sell' ? order._sum.quantity : ''}</span>
                   </td>
-                  <td>{order.price}</td>
+                  <td className="price-cell">{order.price}</td>
                   <td className="order-cell">
                     <div className={`bar buy-bar ${order.type === 'buy' ? 'visible' : ''}`} style={{ width: `${order.type === 'buy' ? order.barLength : 0}%` }}></div>
                     <span className={`order-quantity ${order.type === 'buy' ? 'order-quantity-buy' : ''}`}>{order.type === 'buy' ? order._sum.quantity : ''}</span>
