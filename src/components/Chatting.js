d0559ccdc0130ee1ff479e6c9bb89d69e57cfe31
norwegianwood_97 <jhchoi1997@gmail.com>
2024-04-05 20:08:42 +0900

    FEAT: getMessageFromServer Func initialized

--- src/components/Chatting.js
+++ src/components/Chatting.js
@@ -14,9 +14,27 @@ const Chat = () => {
     ws.current.onclose = () => console.log('Disconnected from the WS server');
     ws.current.onmessage = (e) => {
       const message = JSON.parse(e.data);
-      // 서버로부터 받은 메시지에 현재 시간 추가
-      setMessages((prevMessages) => [...prevMessages, { ...message, isMine: false, timestamp: new Date().toLocaleTimeString() }]);
+
+      // message.type이 'notices'인 경우 처리
+      if (message.type === 'notices') {
+        // message.notices가 배열인지 확인, 배열이 아니면 배열로 변환
+        const noticesArray = Array.isArray(message.notices) ? message.notices : [message.notices];
+
+        // 배열을 사용하여 메시지 처리
+        const newMessages = noticesArray.map((notice) => ({
+          text: notice,
+          isMine: false, // 이 메시지는 사용자 자신의 것이 아니라는 것을 나타냅니다.
+          timestamp: new Date().toLocaleTimeString(), // 메시지에 현재 시간 추가
+        }));
+
+        // 새 메시지 배열을 기존 메시지 목록에 추가
+        setMessages((prevMessages) => [...prevMessages, ...newMessages]);
+      } else {
+        // message.type이 'notices'가 아닌 다른 메시지 처리
+        setMessages((prevMessages) => [...prevMessages, { ...message, isMine: false, timestamp: new Date().toLocaleTimeString() }]);
+      }
     };
+    
 
     return () => {
       ws.current.close();
