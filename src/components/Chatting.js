4b6b548867ab1fd6e326d4b0e88a0f84adb09722
jovid_18 <cshcho99@gmail.com>
2024-04-06 19:20:09 +0900

    TEST: test wss

--- src/components/Chatting.js
+++ src/components/Chatting.js
@@ -8,7 +8,7 @@ const Chat = () => {
   const ws = useRef(null);
 
   useEffect(() => {
-    ws.current = new WebSocket('ws://localhost:3000/ws/chatting');
+    ws.current = new WebSocket(`wss://api.stockingchallenge.site/ws/chartData/${companyId}`);
 
     ws.current.onopen = () => console.log('Connected to the WS server');
     ws.current.onclose = () => console.log('Disconnected from the WS server');
